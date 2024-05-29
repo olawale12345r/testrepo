030f58b0630f60c30e798d705f8933173d345484
olawale12345r <64484864+olawale12345r@users.noreply.github.com>
2024-05-29 12:21:04 -0500

    Create firstpython.py
    adding new file

---
+++ firstpython.py
@@ -0,0 +1,26 @@
+#This code print the stattement how are you doing
+print ('How are you doing!')
+
+#declaring set and variables
+myval=9
+if myval>0:
+    print("it is a positive number")
+else:("it is a negative number")
+
+#declaring array and using a for-loop
+mylist= ["mike", "john", "sam"]
+for x in mylist:
+    print (x)
+    if x == "john":
+        break
+
+mynumbers=[20, 60, 30, 80, 100]
+for y in mynumbers:
+    print(y)
+
+
+shpping_list=["bread", "water", "chips", "candy", "soda"]
+for w in shpping_list:
+    print (w)
+    if w == "chips":
+        break
